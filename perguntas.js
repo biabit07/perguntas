9d38021301c9bec3811d6c7d69b49ab5560ccfa5
biabit07 <00001128113697sp@al.educacao.sp.gov.br>
2024-11-25 14:05:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Qual o valor de pi?',
+    'O valor de pi é 3,14'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'História',
+    'Quem descobriu o Brasil?',
+    'Quem descobriu o Brasil foi Pedro Alvares Cabral'
+)
+
+criaCartao(
+    'Inglês',
+    'Como se diz "cachorro" em inglês?',
+    'Cachorro em inglês se diz "dog"'
+)
